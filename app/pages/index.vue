40dbde3e589a99ccced463a70a23008a578c4ada
Your Name <you@example.com>
2026-09-08 14:04:54 +0800

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="flex min-h-[60vh] items-center justify-center bg-gray-50">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-gray-900">Hallo Yus & Arma</h1>
+    </div>
+  </section>
+</template>
