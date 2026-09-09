88fd92c5d112d5cf5c688fa44e1b896fd22b493c
I Made Arma Dewangga <made.armadewangga@gmail.com>
2026-09-09 10:35:11 +0800

    mengubah warna background dari warna biru jadi coklat kopi

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,16 +4,11 @@
     <section class="min-h-screen px-6 py-20">
       <div class="mx-auto max-w-7xl">
 
-        <h1 class="text-4xl font-bold">
-          Explore Dalat
-        </h1>
-
-        <p class="mt-4 text-gray-600">
-          4 Days 3 Nights Travel Experience
-        </p>
-
+      
       </div>
     </section>
+
+    <Footer />
 
   </div>
 </template>
